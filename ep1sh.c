d13e021203ee8deb065c65e6607afc7877707071
Thomas Ryu <thomasryusameshima@gmail.com>
2015-09-01 17:30:21 -0300

    correcao da impressao do prompt

--- ep1sh.c
+++ ep1sh.c
@@ -7,21 +7,20 @@ int main(int argc, char **argv, char **envp) {
 	pid_t pid;
 	int i;
 	int status;
+    char path[1024];
     char prompt[1024];
-	//char *prompt;
-    //char input[1024];
 	char *input;
     char *token;
     char *command;
     char *parameters[1024];
 
     while (42) {	// inicio do shell
-    	if (getcwd(prompt, sizeof(prompt)) != NULL) { // imprime o path no prompt entre colchetes
- 			//printf("[%s] ", prompt); 
-			int n = strlen(prompt);
-			prompt[n] = ' ';
-			prompt[n+1] = '\0';
+    	if (getcwd(path, sizeof(path)) != NULL) {
+    		strcpy(prompt, "");
+			strcat(strcat(strcat(prompt, "["), path), "] ");
 			input = readline(prompt);
+			if (input[strlen(input) - 1] == '\n')
+ 				input[strlen(input) - 1] = '\0';
 			if(input != NULL) {
 				add_history(input);
 				token = strtok(input, " ");
@@ -38,20 +37,20 @@ int main(int argc, char **argv, char **envp) {
  		else
  			perror("getcwd()");
 
- 		if (strcmp(command, "cd") == 0) {  // cd <novo diretorio para mudar>
+ 		if (strcmp(command, "cd") == 0) {       // cd <novo diretorio para mudar>
  			if (parameters[1] != NULL)
  				if (chdir(parameters[1]) != 0)
  					perror("chdir()");
 		}
  		else if (strcmp(command, "pwd") == 0) { // pwd
-	 		printf("%s\n", prompt);
+	 		printf("%s\n", path);
 		}
- 		else                             // ./ep1 <argumentos do EP1>
+ 		else                                    // ./ep1 <argumentos do EP1>
  			switch (pid = fork()) {
 				case -1: // erro
 					perror("fork()");
 					exit(EXIT_FAILURE);
-				case 0: // estamos em um processo filho
+				case 0:  // estamos em um processo filho
 					status = execve(command, parameters, 0);
 					exit(status);
 				default: // estamos em um processo pai
